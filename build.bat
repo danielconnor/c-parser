d5635c7f133d64a3b31e3900a48278e9ac58f3c9
root <root@danielconnor.(none)>
2013-03-02 12:51:43 +0000

    update build file to use clang and include llvm config

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-g++ -o parser.exe src/main.cc src/token.cc src/scanner.cc src/parser.cc src/scope.cc src/buffer.cc src/ast.cc
+clang++ -g src/main.cc src/token.cc src/scanner.cc src/parser.cc src/scope.cc src/buffer.cc src/ast.cc `llvm-config --cppflags --ldflags --libs core` -o parser
